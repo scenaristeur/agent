b0440b96e1719d2114dc11cdf96030af9fb9cd3a
Scenaristeur <scenaristeur@gmail.com>
2022-03-22 11:53:40 +0100

    Initial dist subtree commit

---
+++ dist/js/about.3d59b4d7.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkagent"]=self["webpackChunkagent"]||[]).push([[443],{5830:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],s=n(1001),l={},r=(0,s.Z)(l,u,a,!1,null,null,null),c=r.exports}}]);
+//# sourceMappingURL=about.3d59b4d7.js.map
