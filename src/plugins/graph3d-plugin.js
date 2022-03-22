2ba253434dea16a6451b14ee9bb2bc3d8f378fc4
Scenaristeur <scenaristeur@gmail.com>
2022-03-22 13:16:54 +0100

    graph plugin

--- src/plugins/graph3d-plugin.js
+++ src/plugins/graph3d-plugin.js
@@ -1,20 +1,47 @@
+let graph = undefined
 const plugin = {
   install(Vue, opts = {}) {
     let store = opts.store
     console.log(store)
 
 
-    Vue.prototype.$graphInit = async function(graph){
+    Vue.prototype.$graphInit = async function(_graph){
+      graph = _graph
       graph.onBackgroundClick(event => onBackgroundClick(event))
+      .onNodeClick(node => onNodeClick(node))
 
       console.log(graph)
     }
 
+
+    async function onNodeClick(node){
+
+      // Aim at node from outside it
+      //  console.log(node)
+      //this.selectedNodes.clear()
+      //this.selectedNodes.has(node) ? this.selectedNodes.delete(node) : this.selectedNodes.add(node);
+      // console.log(this.selectedNodes)
+      // if(node.url != undefined && node.url.startsWith('http')){
+      //   app.$store.commit ('app/mustExplore', node.url)
+      // }
+
+      const distance = 160;
+      const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+
+      graph.cameraPosition(
+        { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+        node, // lookAt ({ x, y, z })
+        3000  // ms transition duration
+      );
+      store.commit ('core/setCurrentNode', node)
+
+    }
+
     async function onBackgroundClick(event){
 
-        console.log("onBackgroundClick", event)
-        store.commit('core/setCurrentNode', null)
-        console.log("new")
+      console.log("onBackgroundClick", event)
+      store.commit('core/setCurrentNode', null)
+      console.log("new")
       //  Vue.prototype.$bvModal.show("modal-node")
 
     }
