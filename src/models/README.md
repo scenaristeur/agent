e990dd9150b3b0ad88a12176ceefc14cbe2bd24d
Scenaristeur <scenaristeur@gmail.com>
2022-06-07 12:07:38 +0200

    layout

--- src/models/README.md
+++
@@ -1,3 +0,0 @@
-# Neurones are the most atomic Entities
-# Brains are groups of Neurones
-# Synapse connects Neurones & Brains
--- src/neurone-factory/index.js
+++
@@ -1,6 +0,0 @@
-//import * as Constants from "./constants/index.js";
-export * from './core.js'
-export * from './brain.js'
-export * from './neurone.js'
-export * from './graph.js'
-export * from './command.js'
