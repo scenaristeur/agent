a2114065ec837a6393cb1b61685e723c00b0a278
scenaristeur <scenaristeur@gmail.com>
2026-01-04 16:17:22 +0100

    yarray

--- src/views/ExperimentsView.vue
+++ src/views/ExperimentsView.vue
@@ -1,17 +1,24 @@
 <template>
     <div>
-        <button @click="init">Init</button> <button @click="add">Add</button> <button @click="addNested">Add
+        Graph:
+        <button @click="init">Init</button>
+        <!-- <button @click="add">Add</button> -->
+        <button @click="addNode">Add node</button>
+        <button @click="logNodes">Log Nodes</button>
+        YJS :
+        <!-- <button @click="addNested">Add
             Nested</button>
         <button @click="setProp">Set Prop</button>
         <button @click="getProp">Get Prop</button>
-        <button @click="deleteProp">Delete Prop</button>
+        <button @click="deleteProp">Delete Prop</button> -->
+
         <div id="3d-graph"></div>
     </div>
 </template>
 
 <script>
 import ForceGraph3D from '3d-force-graph';
-// import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import * as Y from "yjs";
 import { HocuspocusProvider } from '@hocuspocus/provider'
 
@@ -21,37 +28,97 @@ export default {
     },
     data() {
         return {
-            room: "graphs",
-            server: "ws://localhost:1234",
-            connected: false,
             graph: null,
+            nodes: [],
+            links: [],
+            node0: { id: 0, name: "Origin", "val": 10, color: "red" },
             ydoc: null,
             provider: null,
-            ymap_brain: null
+            // ymap_brain: null,
+            room: "example-room",
+            server: "ws://127.0.0.1:1234",
+            connected: false,
         }
     },
     mounted() {
         this.init()
     },
     methods: {
+        // GRAPH
         init() {
+            this.initGraph()
+            this.initYJS()
+        },
+        initGraph() {
             // graph from https://gist.github.com/vasturiano/2f602ea6c51c664c29ec56cbe2d6a5f6
-            const initData = {
-                nodes: [{ id: 0 }],
-                links: []
-            }
+            // const initData = {
+            //     nodes: [{ id: 0 }],
+            //     links: []
+            // }
+            this.nodes = []
+            this.links = []
+            this.nodes = [...this.nodes, this.node0]
             const elem = document.getElementById("3d-graph");
             this.graph = ForceGraph3D()(elem)
                 .enableNodeDrag(false)
                 .onNodeHover(node => elem.style.cursor = node ? 'pointer' : null)
                 .onNodeClick(this.removeNode)
-                .graphData(initData);
+                .graphData({ nodes: this.nodes, links: this.links });
+        },
 
+        addNode() {
+            let id = uuidv4()
+            let node = { id: id, name: "name of " + id, }
+            this.nodes = [...this.nodes, node]
+            let link = { source: node.id, target: 0 }
+            console.log("link", link)
+            this.links = [...this.links, link]
+            this.updateGraph()
+            this.yNodes.push([node])
+            this.yLinks.push([link])
+            // this.updateYjs()
+        },
+        add1() {
+            console.log("ExperimentsView: Ajout d'un nouveau noeud");
+            //const { nodes, links } = this.graph.graphData();
+            const id = this.nodes.length;
+            this.nodes = [...this.nodes, { id: id }]
+            let link = { source: id, target: Math.round(Math.random() * (id - 1)) }
+            console.log("link", link)
+            this.links = [...this.links, link]
+            this.updateGraph()
+            this.updateYjs()
+        },
+        removeNode(node) {
+            console.log("ExperimentsView: Suppression d'un noeud");
+            // let { nodes, links } = this.graph.graphData();
+            // console.log("remove ", node.id)
+            this.links = this.links.filter(l => l.source.id !== node.id && l.target.id !== node.id); // Remove links attached to node
+            this.nodes = this.nodes.filter(n => n.id !== node.id); // Remove node
+            this.nodes.forEach((n, idx) => { n.id = idx; }); // Reset node ids to array index
+            this.updateGraph()
+            this.updateYjs()
+            // console.log(this.nodes)
+            // console.log(this.links)
+        },
+        updateGraph() {
+            this.graph.graphData({ nodes: this.nodes, links: this.links });
+
+        },
+
+        logNodes() {
+            console.info(this.nodes)
+            console.info(this.links)
+        },
+
+        //YJS
+
+        initYJS() {
             // yjs from https://tiptap.dev/docs/hocuspocus/provider/install#introduction
             this.ydoc = new Y.Doc()
             this.provider = new HocuspocusProvider({
-                url: 'ws://127.0.0.1:1234',
-                name: 'example-document',
+                url: this.server,
+                name: this.room,
                 document: this.ydoc,
                 // onOpen: this.onOpen,
                 // onConnect: this.onConnect,
@@ -69,48 +136,91 @@ export default {
                 // onStateless: this.onStateless
             })
 
-            this.ymap_brain = this.ydoc.getMap('test')
-        },
-        addNested() {
-            const ymapNested = new Y.Map()
 
-            // Nested types can be included as content into any other shared type
-            this.ymap_brain.set('my nested map', ymapNested)
-        },
-        setProp() {
-            // Common methods
-            let date = Date.now()
-            this.ymap_brain.set('prop-name', date) // value can be anything json-encodable
-            console.log("added date ", date)
-        },
-        getProp() {
-            // Common methods
-            let d = this.ymap_brain.get('prop-name') // => 'value'
-            console.log("date stored is", d, "but date is ", Date.now())
-        },
-        deleteProp() {
-            // Common methods
 
-            this.ymap_brain.delete('prop-name')
-            console.log("deleted")
+
+
+            // this.ymap_brain = this.ydoc.getMap('test')
+            this.yNodes = this.ydoc.getArray('nodes')
+            this.yLinks = this.ydoc.getArray('links')
+            this.yNodes.push([this.node0])
+
+            this.yNodes.observe(yarrayEvent => {
+                // yarrayEvent.target === yarray // => true
+
+                // Find out what changed: 
+                // Log the Array-Delta Format to calculate the difference to the last observe-event
+                console.log("nodes changes", yarrayEvent.changes.delta)
+                console.log(yarrayEvent.changes)
+            })
+            this.yLinks.observe(yarrayEvent => {
+                // yarrayEvent.target === yarray // => true
+
+                // Find out what changed: 
+                // Log the Array-Delta Format to calculate the difference to the last observe-event
+                console.log("links changes", yarrayEvent.changes.delta)
+                console.log(yarrayEvent.changes)
+            })
+
+            // this.ymap_brain.unobserve()
+            // this.ymap_brain.observe(ymapEvent => {
+            //     // ymapEvent.target === ymap_brain // => true
+
+            //     // Find out what changed: 
+            //     // Option 1: A set of keys that changed
+            //     // console.log(ymapEvent.keysChanged) // => Set<strings>
+            //     // Option 2: Compute the differences
+            //     // console.log(ymapEvent.changes.keys) // => Map<string, { action: 'add'|'update'|'delete', oldValue: any}>
+
+            //     // // sample code.
+            //     ymapEvent.changes.keys.forEach((change, key) => {
+            //         if (change.action === 'add') {
+            //             console.log(`Property "${key}" was added. Initial value: "${JSON.stringify(this.ymap_brain.get(key))}".`)
+            //         } else if (change.action === 'update') {
+            //             console.log(`Property "${key}" was updated. New value: "${JSON.stringify(this.ymap_brain.get(key))}". Previous value: "${JSON.stringify(change.oldValue)}".`)
+            //         } else if (change.action === 'delete') {
+            //             console.log(`Property "${key}" was deleted. New value: undefined. Previous value: "${JSON.stringify(change.oldValue)}".`)
+            //         }
+            //     })
+
+            //     // console.info(this.ymap_brain.toJSON())
+            // })
+
+
+
+            this.updateYjs()
         },
-        add() {
-            console.log("ExperimentsView: Ajout d'un nouveau noeud");
-            const { nodes, links } = this.graph.graphData();
-            const id = nodes.length;
-            this.graph.graphData({
-                nodes: [...nodes, { id }],
-                links: [...links, { source: id, target: Math.round(Math.random() * (id - 1)) }]
-            });
+        updateYjs() {
+            // let graph = { nodes: this.nodes, links: this.links }
+            // console.log(graph)
+            // this.ymap_brain.set('nodes', this.nodes)
+            // this.ymap_brain.set('links', this.links)
         },
-        removeNode(node) {
-            console.log("ExperimentsView: Suppression d'un noeud");
-            let { nodes, links } = this.graph.graphData();
-            links = links.filter(l => l.source !== node.id && l.target !== node.id); // Remove links attached to node
-            nodes = nodes.filter(n => n.id !== node.id); // Remove node
-            nodes.forEach((n, idx) => { n.id = idx; }); // Reset node ids to array index
-            this.graph.graphData({ nodes, links });
-        },
+
+        // addNested() {
+        //     const ymapNested = new Y.Map()
+
+        //     // Nested types can be included as content into any other shared type
+        //     this.ymap_brain.set('my nested map', ymapNested)
+        // },
+        // setProp() {
+        //     // Common methods
+        //     let date = Date.now()
+        //     this.ymap_brain.set('prop-name', date) // value can be anything json-encodable
+        //     console.log("added date ", date)
+        // },
+        // getProp() {
+        //     // Common methods
+        //     let d = this.ymap_brain.get('prop-name') // => 'value'
+        //     console.log("date stored is", d, "but date is ", Date.now())
+        //     console.log(this.ymap_brain.toJSON())
+        // },
+        // deleteProp() {
+        //     // Common methods
+
+        //     this.ymap_brain.delete('prop-name')
+        //     console.log("deleted")
+        // },
         onOpen() {
             console.info("ExperimentsView: Ouverture de la connexion");
         },
